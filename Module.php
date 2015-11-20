10bf9cf3dbb3cd30e29da059d778d3c55573cde0
Maris Graudins <maris.graudins91@gmail.com>
2015-11-20 12:33:56 +0200

    Fixes #67: Ability to change permissions for debugger data files and directories

--- Module.php
+++ Module.php
@@ -58,6 +58,21 @@ class Module extends \yii\base\Module implements BootstrapInterface
      */
     public $dataPath = '@runtime/debug';
     /**
+     * @var integer the permission to be set for newly created debugger data files.
+     * This value will be used by PHP [[chmod()]] function. No umask will be applied.
+     * If not set, the permission will be determined by the current environment.
+     * @since 2.0.6
+     */
+    public $fileMode;
+    /**
+     * @var integer the permission to be set for newly created directories.
+     * This value will be used by PHP [[chmod()]] function. No umask will be applied.
+     * Defaults to 0775, meaning the directory is read-writable by owner and group,
+     * but read-only for other users.
+     * @since 2.0.6
+     */
+    public $dirMode = 0775;
+    /**
      * @var integer the maximum number of debug data files to keep. If there are more files generated,
      * the oldest ones will be removed.
      */
