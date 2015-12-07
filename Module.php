eb0542d949a17ee050a31b91c6702d3c74dd4c56
lichunqiang <light-li@hotmail.com>
2015-12-07 19:12:02 +0800

    Restore original configurated class after build rules

--- Module.php
+++ Module.php
@@ -144,12 +144,14 @@ class Module extends \yii\base\Module implements BootstrapInterface
         $app->on(Application::EVENT_BEFORE_REQUEST, function () use ($app) {
             $app->getView()->on(View::EVENT_END_BODY, [$this, 'renderToolbar']);
         });
-        $urlManager = $app->getUrlManager();
-        $urlManager->ruleConfig['class'] = 'yii\web\UrlRule';
-        $urlManager->addRules([
+        $urlManger = $app->getUrlManager();
+        $originConfigClass = $urlManger->ruleConfig['class'];
+        $urlManger->ruleConfig['class'] = 'yii\web\UrlRule';
+        $urlManger->addRules([
             $this->id => $this->id,
             $this->id . '/<controller:[\w\-]+>/<action:[\w\-]+>' => $this->id . '/<controller>/<action>',
         ], false);
+        $urlManger->ruleConfig['class'] = $originConfigClass;
     }
 
     /**
