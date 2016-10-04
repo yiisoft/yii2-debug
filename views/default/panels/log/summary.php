f01e61368ec15661ea6967b389827dd57d48214e
rhertogh <rhertogh@users.noreply.github.com>
2016-10-04 14:27:32 +0200

    Update summary.php
    Only show number of errors or warnings on the respective link and pluralize labels according to their count.

--- views/default/panels/log/summary.php
+++ views/default/panels/log/summary.php
@@ -8,30 +8,30 @@ use yii\log\Logger;
 ?>
 
 <?php
-$title = 'Logged ' . count($data['messages']) . ' messages';
+$titles = ['all' => Yii::$app->i18n->format('Logged {n,plural,=1{1 message} other{# messages}}', ['n' => count($data['messages'])], 'en-US')];
 $errorCount = count(Target::filterMessages($data['messages'], Logger::LEVEL_ERROR));
 $warningCount = count(Target::filterMessages($data['messages'], Logger::LEVEL_WARNING));
 
-if ($errorCount) {
-    $title .= ", $errorCount errors";
+if ($errorCount){
+    $titles['errors'] = Yii::$app->i18n->format('{n,plural,=1{1 error} other{# errors}}', ['n' => $errorCount], 'en-US');
 }
 
 if ($warningCount) {
-    $title .= ", $warningCount warnings";
+    $titles['warnings'] = Yii::$app->i18n->format('{n,plural,=1{1 warning} other{# warnings}}', ['n' => $warningCount], 'en-US');;
 }
 ?>
 
 <div class="yii-debug-toolbar__block">
-    <a href="<?= $panel->getUrl() ?>" title="<?= $title ?>">Log
+    <a href="<?= $panel->getUrl() ?>" title="<?= implode(',&nbsp;', $titles) ?>">Log
         <span class="yii-debug-toolbar__label"><?= count($data['messages']) ?></span>
     </a>
     <?php if ($errorCount): ?>
-    <a href="<?= $panel->getUrl(['Log[level]' => Logger::LEVEL_ERROR])?>" title="<?= $title ?>">
+    <a href="<?= $panel->getUrl(['Log[level]' => Logger::LEVEL_ERROR])?>" title="<?= $titles['errors'] ?>">
         <span class="yii-debug-toolbar__label yii-debug-toolbar__label_important"><?= $errorCount ?></span>
     </a>
     <?php endif; ?>
     <?php if ($warningCount): ?>
-    <a href="<?= $panel->getUrl(['Log[level]' => Logger::LEVEL_WARNING])?>" title="<?= $title ?>">
+    <a href="<?= $panel->getUrl(['Log[level]' => Logger::LEVEL_WARNING])?>" title="<?= $titles['warnings'] ?>">
         <span class="yii-debug-toolbar__label yii-debug-toolbar__label_warning"><?= $warningCount ?></span>
     </a>
     <?php endif; ?>
