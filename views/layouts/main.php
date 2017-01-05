5f09b7ccdab3990a58e6a6b0b9cae96501ab7fed
Alexander Makarov <sam@rmcreative.ru>
2017-01-05 10:18:15 +0300

    Unnecessary formatting change

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -17,7 +17,6 @@ yii\debug\DebugAsset::register($this);
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
-
 </head>
 <body>
 <?php $this->beginBody() ?>
