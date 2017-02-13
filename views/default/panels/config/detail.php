2681e3a2c766c45a7415c961c97b6afb619b4970
Carsten Brandt <mail@cebe.cc>
2017-02-13 21:29:04 +0100

    Fixes #196: Added language information to config panel

--- views/default/panels/config/detail.php
+++ views/default/panels/config/detail.php
@@ -5,14 +5,26 @@ $extensions = $panel->getExtensions();
 <h1>Configuration</h1>
 
 <?php
+$formatLanguage = function($locale) {
+    if (class_exists('Locale', false)) {
+        $region = Locale::getDisplayLanguage($locale, 'en');
+        $language = Locale::getDisplayRegion($locale, 'en');
+        return ' (' . implode(',', array_filter([$language, $region])) . ')';
+    }
+    return '';
+};
+$app = $panel->data['application'];
 echo $this->render('table', [
     'caption' => 'Application Configuration',
     'values' => [
-        'Yii Version' => $panel->data['application']['yii'],
-        'Application Name' => $panel->data['application']['name'],
-        'Application Version' => $panel->data['application']['version'],
-        'Environment' => $panel->data['application']['env'],
-        'Debug Mode' => $panel->data['application']['debug'] ? 'Yes' : 'No',
+        'Yii Version' => $app['yii'],
+        'Application Name' => $app['name'],
+        'Application Version' => $app['version'],
+        'Current Language' => !empty($app['language']) ? $app['language'] . $formatLanguage($app['language']) : '',
+        'Source Language' => !empty($app['sourceLanguage']) ? $app['sourceLanguage'] . $formatLanguage($app['sourceLanguage']) : '',
+        'Charset' => !empty($app['charset']) ? $app['charset'] : '',
+        'Environment' => $app['env'],
+        'Debug Mode' => $app['debug'] ? 'Yes' : 'No',
     ],
 ]);
 
