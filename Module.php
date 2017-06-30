b0057c8eda8ad0b082e7b76c05c612f999b84192
Daniel Gómez Pan <pana_1990@hotmail.com>
2017-06-30 18:09:14 +0200

    Fixes #239: Fixed an issue in the user panel when using console application with debug module enabled

--- Module.php
+++ Module.php
@@ -142,6 +142,10 @@ class Module extends \yii\base\Module implements BootstrapInterface
      */
     public function init()
     {
+        if (!Yii::$app instanceof \yii\web\Application) {
+            return;
+        }
+
         parent::init();
         $this->dataPath = Yii::getAlias($this->dataPath);
         $this->initPanels();
