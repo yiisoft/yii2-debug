59b1284b4abf2ae15a603dde047b0ffb6be9700c
Mehdi Achour <machour@gmail.com>
2019-03-23 10:39:39 +0100

    Compact output

--- src/assets/build.sh
+++ src/assets/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-for file in main toolbar timeline; do sass scss/$file.scss css/$file.css --sourcemap=none; done
+for file in main toolbar timeline; do sass scss/$file.scss css/$file.css --sourcemap=none --style=compressed; done
