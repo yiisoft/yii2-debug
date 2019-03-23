a38b16e56618114157c871b72c6d6d081f020bd8
Mehdi Achour <machour@gmail.com>
2019-03-23 10:27:23 +0100

    Switch to bootstrap 4.3.1 and fix some layout issues

--- src/views/default/index.php
+++ src/views/default/index.php
@@ -27,136 +27,134 @@ $this->title = 'Yii Debugger';
     </div>
 
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <h1>Available Debug Data</h1>
-                <?php
+        <div class="table-responsive">
+            <h1>Available Debug Data</h1>
+            <?php
 
-                $codes = [];
-                foreach ($manifest as $tag => $vals) {
-                    if (!empty($vals['statusCode'])) {
-                        $codes[] = $vals['statusCode'];
-                    }
+            $codes = [];
+            foreach ($manifest as $tag => $vals) {
+                if (!empty($vals['statusCode'])) {
+                    $codes[] = $vals['statusCode'];
                 }
-                $codes = array_unique($codes, SORT_NUMERIC);
-                $statusCodes = !empty($codes) ? array_combine($codes, $codes) : null;
+            }
+            $codes = array_unique($codes, SORT_NUMERIC);
+            $statusCodes = !empty($codes) ? array_combine($codes, $codes) : null;
 
-                $hasDbPanel = isset($panels['db']);
+            $hasDbPanel = isset($panels['db']);
 
-                echo GridView::widget([
-                    'dataProvider' => $dataProvider,
-                    'filterModel' => $searchModel,
-                    'rowOptions' => function ($model) use ($searchModel, $hasDbPanel) {
-                        if ($searchModel->isCodeCritical($model['statusCode'])) {
-                            return ['class' => 'danger'];
-                        }
+            echo GridView::widget([
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'rowOptions' => function ($model) use ($searchModel, $hasDbPanel) {
+                    if ($searchModel->isCodeCritical($model['statusCode'])) {
+                        return ['class' => 'danger'];
+                    }
 
-                        if ($hasDbPanel && $this->context->module->panels['db']->isQueryCountCritical($model['sqlCount'])) {
-                            return ['class' => 'danger'];
-                        }
+                    if ($hasDbPanel && $this->context->module->panels['db']->isQueryCountCritical($model['sqlCount'])) {
+                        return ['class' => 'danger'];
+                    }
 
-                        return [];
-                    },
-                    'pager' => [
-                        'linkContainerOptions' => [
-                            'class' => 'page-item'
-                        ],
-                        'linkOptions' => [
-                            'class' => 'page-link'
-                        ],
-                        'disabledListItemSubTagOptions' => [
-                            'tag' => 'a',
-                            'href' => 'javascript:;',
-                            'tabindex' => '-1',
-                            'class' => 'page-link'
+                    return [];
+                },
+                'pager' => [
+                    'linkContainerOptions' => [
+                        'class' => 'page-item'
+                    ],
+                    'linkOptions' => [
+                        'class' => 'page-link'
+                    ],
+                    'disabledListItemSubTagOptions' => [
+                        'tag' => 'a',
+                        'href' => 'javascript:;',
+                        'tabindex' => '-1',
+                        'class' => 'page-link'
+                    ]
+                ],
+                'columns' => array_filter([
+                    ['class' => 'yii\grid\SerialColumn'],
+                    [
+                        'attribute' => 'tag',
+                        'value' => function ($data) {
+                            return Html::a($data['tag'], ['view', 'tag' => $data['tag']]);
+                        },
+                        'format' => 'html',
+                    ],
+                    [
+                        'attribute' => 'time',
+                        'value' => function ($data) {
+                            return '<span class="nowrap">' . Yii::$app->formatter->asDatetime($data['time'],
+                                    'yyyy-MM-dd HH:mm:ss') . '</span>';
+                        },
+                        'format' => 'html',
+                    ],
+                    'ip',
+                    $hasDbPanel ? [
+                        'attribute' => 'sqlCount',
+                        'label' => 'Query Count',
+                        'value' => function ($data) {
+                            /* @var $dbPanel \yii\debug\panels\DbPanel */
+                            $dbPanel = $this->context->module->panels['db'];
+
+                            if ($dbPanel->isQueryCountCritical($data['sqlCount'])) {
+                                $content = Html::tag('b', $data['sqlCount']) . ' ' . Html::tag('span', '&#x26a0;');
+
+                                return Html::a($content, ['view', 'panel' => 'db', 'tag' => $data['tag']], [
+                                    'title' => 'Too many queries. Allowed count is ' . $dbPanel->criticalQueryThreshold,
+                                ]);
+
+                            }
+                            return $data['sqlCount'];
+                        },
+                        'format' => 'html',
+                    ] : null,
+                    [
+                        'attribute' => 'mailCount',
+                        'visible' => isset($this->context->module->panels['mail']),
+                    ],
+                    [
+                        'attribute' => 'method',
+                        'filter' => [
+                            'get' => 'GET',
+                            'post' => 'POST',
+                            'delete' => 'DELETE',
+                            'put' => 'PUT',
+                            'head' => 'HEAD'
                         ]
                     ],
-                    'columns' => array_filter([
-                        ['class' => 'yii\grid\SerialColumn'],
-                        [
-                            'attribute' => 'tag',
-                            'value' => function ($data) {
-                                return Html::a($data['tag'], ['view', 'tag' => $data['tag']]);
-                            },
-                            'format' => 'html',
-                        ],
-                        [
-                            'attribute' => 'time',
-                            'value' => function ($data) {
-                                return '<span class="nowrap">' . Yii::$app->formatter->asDatetime($data['time'],
-                                        'yyyy-MM-dd HH:mm:ss') . '</span>';
-                            },
-                            'format' => 'html',
-                        ],
-                        'ip',
-                        $hasDbPanel ? [
-                            'attribute' => 'sqlCount',
-                            'label' => 'Query Count',
-                            'value' => function ($data) {
-                                /* @var $dbPanel \yii\debug\panels\DbPanel */
-                                $dbPanel = $this->context->module->panels['db'];
-
-                                if ($dbPanel->isQueryCountCritical($data['sqlCount'])) {
-                                    $content = Html::tag('b', $data['sqlCount']) . ' ' . Html::tag('span', '&#x26a0;');
-
-                                    return Html::a($content, ['view', 'panel' => 'db', 'tag' => $data['tag']], [
-                                        'title' => 'Too many queries. Allowed count is ' . $dbPanel->criticalQueryThreshold,
-                                    ]);
-
-                                }
-                                return $data['sqlCount'];
-                            },
-                            'format' => 'html',
-                        ] : null,
-                        [
-                            'attribute' => 'mailCount',
-                            'visible' => isset($this->context->module->panels['mail']),
-                        ],
-                        [
-                            'attribute' => 'method',
-                            'filter' => [
-                                'get' => 'GET',
-                                'post' => 'POST',
-                                'delete' => 'DELETE',
-                                'put' => 'PUT',
-                                'head' => 'HEAD'
-                            ]
-                        ],
-                        [
-                            'attribute' => 'ajax',
-                            'value' => function ($data) {
-                                return $data['ajax'] ? 'Yes' : 'No';
-                            },
-                            'filter' => ['No', 'Yes'],
-                        ],
-                        [
-                            'attribute' => 'url',
-                            'label' => 'URL',
-                        ],
-                        [
-                            'attribute' => 'statusCode',
-                            'value' => function ($data) {
-                                $statusCode = $data['statusCode'];
-                                if ($statusCode === null) {
-                                    $statusCode = 200;
-                                }
-                                if ($statusCode >= 200 && $statusCode < 300) {
-                                    $class = 'label-success';
-                                } elseif ($statusCode >= 300 && $statusCode < 400) {
-                                    $class = 'label-info';
-                                } else {
-                                    $class = 'label-danger';
-                                }
-                                return "<span class=\"label {$class}\">$statusCode</span>";
-                            },
-                            'format' => 'raw',
-                            'filter' => $statusCodes,
-                            'label' => 'Status code'
-                        ],
-                    ]),
-                ]);
-                ?>
-            </div>
+                    [
+                        'attribute' => 'ajax',
+                        'value' => function ($data) {
+                            return $data['ajax'] ? 'Yes' : 'No';
+                        },
+                        'filter' => ['No', 'Yes'],
+                    ],
+                    [
+                        'attribute' => 'url',
+                        'label' => 'URL',
+                    ],
+                    [
+                        'attribute' => 'statusCode',
+                        'value' => function ($data) {
+                            $statusCode = $data['statusCode'];
+                            if ($statusCode === null) {
+                                $statusCode = 200;
+                            }
+                            if ($statusCode >= 200 && $statusCode < 300) {
+                                $class = 'label-success';
+                            } elseif ($statusCode >= 300 && $statusCode < 400) {
+                                $class = 'label-info';
+                            } else {
+                                $class = 'label-danger';
+                            }
+                            return "<span class=\"label {$class}\">$statusCode</span>";
+                        },
+                        'format' => 'raw',
+                        'filter' => $statusCodes,
+                        'label' => 'Status code'
+                    ],
+                ]),
+            ]);
+            ?>
         </div>
     </div>
 </div>
