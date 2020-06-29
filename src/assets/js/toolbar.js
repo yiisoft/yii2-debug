a02922c593477ba07f7eecd0ab88e3c52c67c50b
Ivan Chivchibashiev <naduvko@gmail.com>
2020-06-29 16:06:24 +0300

    Revert "Rewrite to use element data attributes"
    This reverts commit 4bf1c1f5e84047ed79930e6dc0277e426fad7f93.

--- src/assets/js/toolbar.js
+++ src/assets/js/toolbar.js
@@ -322,9 +322,9 @@
     function shouldTrackRequest(requestUrl) {
         var a = document.createElement('a');
         a.href = requestUrl;
-        var skipAjaxRequestUrls = JSON.parse(toolbarEl.getAttribute('data-skip-urls'))
-        if (skipAjaxRequestUrls) {
-            if (skipAjaxRequestUrls.includes(requestUrl)) {
+        if (window.hasOwnProperty("skipAjaxRequestUrl")) {
+            var requestRoute = requestUrl.split('?')
+            if (skipAjaxRequestUrl.includes(requestRoute[0])) {
                 return false;
             }
         }
