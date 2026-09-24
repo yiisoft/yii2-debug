a2e213930bff2c2c105ce3c0f6f44dc61bc1f705
Maksim Spirkov <63721828+mspirkov@users.noreply.github.com>
2026-09-24 17:15:34 +0300

    Check the code style fully on CI (#567)

--- tests/router/module/Module.php
+++ tests/router/module/Module.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @link https://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license https://www.yiiframework.com/license/
+ */
+
 namespace yiiunit\debug\router\module;
 
 class Module extends \yii\base\Module
